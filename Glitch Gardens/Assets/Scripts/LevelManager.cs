a5e158d28942aaf254814101b43d21cbdc677e69
Charles Pence <charles@fanboatstudios.com>
2017-09-26 00:42:00 -0500

    Getting the splash screen and start screens setup with level manager and persistent music manager

---
+++ Glitch Gardens/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+		print ("Level Manger Starting...");
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name){
+		print ("New Level load: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest(){
+		print ("Quit requested");
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+		print ("Loading next level");
+		int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+		SceneManager.LoadScene(nextSceneIndex);
+    }
+
+}
---
+++ Glitch Gardens/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake(){
+		GameObject.DontDestroyOnLoad(gameObject);
+		print ("Don't destroy on load: " + name);
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnSceneLoaded (Scene scene, LoadSceneMode mode){
+		AudioClip currentLevelMusic = levelMusicChangeArray [scene.buildIndex];
+		print ("Playing Clip: " + currentLevelMusic);
+		if (currentLevelMusic) {
+			audioSource.clip = currentLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+}
